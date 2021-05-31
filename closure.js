18885125d6e16df4d271e76790926c944f695a9f
Habibur Rahman <rhabibur1710@gmail.com>
2021-05-31 19:07:58 +0600

    Closure

---
+++ closure.js
@@ -0,0 +1,9 @@
+//when a we use outsider scope inside any block that is called closure
+
+function a(){
+    var x=10
+return function(){
+    console.log(x)
+}
+}
+console.dir(a())
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //Js is not a Functional Language
 //Js accept all the trems of functional language
 //What is Fuctional Language?
-//Ans: Functional Language is a programming paradiagram - a style of building structure and elments of  computer programs as the evaluation of mathematical functions avoiding changing  states and mutable data --wikipedia
+//Ans: Functional Language is a programming paradiagram - a style of building structure and elments of
+//computer programs as the evaluation of mathematical functions avoiding changing  states and mutable data --wikipedia
 
 //Three main terms of Functional Programming
 /*
