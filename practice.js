47b6383b7ac4ef7ae5cf99d39eaf6ca8e29b2647
Habibur Rahman <rhabibur1710@gmail.com>
2021-06-15 12:51:57 +0600

    Practice js

--- practice.js
+++ practice.js
@@ -1,186 +1,183 @@
-// //Pure function 
-// var sqr=function (a){
-//     return a*a
-// }
-// console.log(sqr(2))
+//pure function
 
-// //first class function
+function sqr(a){
+    return a*a
+}
+console.log(sqr(4))
 
-// //function can be stored in a variable
-// var sub = function(a,b){
-//     return a-b
-// }
-// console.log(sub(10,5))
 
-// //function can be stored in an arry
-// var arr=[
-//     sqr(3),sub(10,5)
-// ]
+//first class function
 
-// console.log(arr)
-// console.log(typeof arr)
+//1. function can be store in a variable
 
-// //function can be stored in an object
+var addition=function(a,b){
+    return a+b
+}
+console.log(typeof addition)
 
-// var obj={x:sqr(4),y:sub(50,20)}
-// console.log(obj)
+var result=addition(4,5)
+console.log(result)
 
-//function can be passed as an arguments
-// function add(a,b){
-//     return a+b
-// }
-// function mul(a,b,func){
-//     return a*b*func(a,b)
-// }
+//2.function can be stored in an array
 
-// console.log(mul(5,5,add))
+var  math=[addition(4,5),10,11]
+console.log(math)
 
+//3.function can be stored in an object
+var sub = function(a,b){
+    return a-b
+}
+var obj={
+    addition:addition(4,5),substration:sub(5,4)
+}
+console.log(obj)
+
+//4.function can created anywhere needed
+function hello(){
+    console.log("Hello World!!!"),
+
+    setTimeout(function(){
+        console.log("hello function executed")
+    },10)
+}
+hello()
+
+//higher order function
+
+//5. function can be return from another function
+
+function add(){
+    return addition(10,15)
+}
+console.log(add())
+
+//6. function can be used as an argument of another function
+
+function calculation(a,b){
+    return a*b
+}
+console.log(calculation(addition(10,25),sub(5,4)))
+
+//closure : when outside scope used inside a block its called closure
+
+var x=10
+function display(){
+    return x
+}
+console.log(display())
 
 //callback function
 
+//forEach Function
+var arr=[1,2,3,4,5,6,7,8,9]
 
-// function gc(grd,crdt,cgcal){
-//     var tgrd=grd
-//     var tc=crdt
-//     return cgcal(tgrd,tc)
+console.log(arr.forEach(function(value,i,n){
+    console.log(value,i,n)
+}))
 
-// }
+//implementing forEach function using callback function
 
-// var result=gc(3,13,function(tgrd,tc){
-//     return (tgrd*tc)/tc
-// })
-// console.log(result)
-
-
-//filter function 
-var arr=[1,5,6,7,8,2,3,4,9,10,15,18,19,60,30,25,22,24,29,23,33,35,34,38,40,56,57,52,51,59]
-
-// oddNumber=arr.filter(function(value){
-//     return value% 2!= 0
-// })
-// console.log(oddNumber)
-
-
-// //implementing filter function without callback function
-// function oddFilter(arr){
-//     var oddArr=[]
-//     for(var i=0;i<arr.length;i++){
-//         if(arr[i]%2 != 0){
-//             oddArr.push(arr[i])
-//         }
-//     }
-//    return oddArr
-// }
-
-// console.log(oddFilter(arr))
-
-// //implementing filter function using call back function
-
-// function evenFilter(arr,cbb){
-//     var evenNum=[]
-//     for(var i=0;i<arr.length;i++){
-//         if(cbb(arr[i])){
-//             evenNum.push(arr[i])
-//         }
-//     }
-//     return evenNum
-// }
-// var result=evenFilter(arr,function(value){
-//     return value %2 ==0
-// })
-// //FILETER FUNCTION
-//  var rslt=arr.filter(function(value){
-//      return value %2==0
-//  })
-//  console.log(rslt)
-
-
-// //map function 
-// console.log(arr.map(function(value){
-//     return value*value
-// }))
-
-// //implementing map function without callback function
-
-// function sqrFunction(arr){
-//     var sqr=[]
-//     for(var i=0;i<arr.length;i++){
-//         sqr.push(arr[i]*arr[i])
-//     }
-//     return sqr
-// }
-
-// console.log(sqrFunction(arr))
-
-// //implementing sqr function using call back function
-
-// function newSqrFun(arr,cb){
-//     var newSqr=[]
-//     for(var i=0;i<arr.length;i++){
-//         newSqr.push(cb(arr[i]))
-//     }
-//     return newSqr
-// }
-
-// console.log(newSqrFun(arr,function(value){
-//     return value * value
-// }))
-
-var num=[1,2,3,4,5]
-// console.log(num.reduce(function(p,c){
-    
-//     return p+c
-// },2))
-
-
-//implementing reduce additon function without callback function
-
-// function additon(a){
-//     var sum=0
-//     for(var i=0;i<a.length;i++){
-//      sum=sum+a[i]
-//     }
-//     return sum
-// }
-
-// console.log(additon(num))
-
-// //implementing reduce function using call back function 
-
-// function myreduce(num,cb,acc){
-//     for(var i=0;i<num.length;i++){
-//         acc=cb(num[i],acc)
-//     }
-//     return acc
-// }
-
-
-// console.log(myreduce(num,function(p,c){
-//     return p+c
-// },0))
-
-// console.log(myreduce(num,function(p,c){
-//     return p*c
-// },0))
-
-// for each function in js
-
-//ForEach Function
-
-num.forEach(function(value,i,arr){
-   console.log(value,i,arr)
-})
-
-//implementing ForEach function with call back function
-function myForEach(num,cb){
-    for(var i=0;i<num.length;i++){
-        
-       var myFun= cb(num[i],i,num)
+function myforEach(arr,cb){
+    for(var i=0;i<arr.length;i++){
+    cb(arr[i],i,arr)
+      
     }
-    return myFun
+}
+myforEach(arr,function(value,i,n){
+    console.log(value,i,n)
+})
+//map function
+
+console.log(arr.map(function(value){
+    return value*2
+}))
+
+//implementation of map function using callback function
+
+function mymap(arr,cb){
+    var ar=[]
+    for(var i=0;i<arr.length;i++){
+        ar.push(cb(arr[i]))
+    }
+    return ar
+}
+console.log(mymap(arr,function(value){
+    return value*3
+}))
+
+//filter function
+
+console.log(arr.filter(function(value){
+    return value%2==0
+}))
+
+//implementation of filter function using call back function
+
+function myfilter(arr,cb){
+    var nfilter=[]
+    for (var index = 0; index < arr.length; index++) {
+      if(cb(arr[index])){
+        nfilter.push(arr[index])
+      } 
+        
+    }
+    return nfilter
+}
+console.log(myfilter(arr,function(value){
+    return value % 2 != 0 
+}))
+
+
+console.log(arr.find(function(value){
+    return value==5
+}))
+console.log(arr.findIndex(function(value){
+    return value==5
+}))
+
+//implementation of find and findIndex using callback function
+
+function myfind(arr,cb){
+    for(var i=0;i<arr.length;i++)
+    {
+        if(cb(arr[i])){
+            return arr[i]
+        }
+    }
 }
 
-var result=myForEach(num,function(value,i,array){
-    console.log(value,i,array)
-})
+console.log(myfind(arr,function(value){
+    return value==5
+}))
+
+function myfindIndex(arr,cb){
+    for(var i=0;i<arr.length;i++)
+    {
+        if(cb(arr[i])){
+            return i
+        }
+    }
+}
+
+console.log(myfindIndex(arr,function(value){
+    return value==5
+}))
+
+//reduce function 
+
+console.log(arr.reduce(function(p,v){
+    return p+v
+}))
+
+
+function arrayAddition(arr,cb){
+    var sum=0
+        for(var i=0;i<arr.length;i++){
+        sum+=arr[i]
+    }
+    return sum
+}
+console.log(arrayAddition(arr,function(p,v){
+    return p+v
+}))
 
